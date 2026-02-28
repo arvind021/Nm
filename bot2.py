946da196929679b9389f478a607fd0b631b1a406
arvind021 <ak4392916@gmail.com>
2026-02-28 20:54:24 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -89,7 +89,7 @@ class UltraBot:
             "API_HASH": "bf8ce0c575b02bde5ce419944b0e7ea8",
             "BOT_TOKEN": "8746899720:AAGEKqRTQyJCXJ2OExmckEYjdhAlz5BZgOk",
             "ADMIN_IDS": [7302427268, 8627378748],
-            "ADMIN_PASSWORD": "100",
+            "ADMIN_PASSWORD": "Smoke@2024!",
             "SESSION_TIMEOUT": 3600,
             "MAX_RETRIES": 5,
             "SPEED_MODE": "balanced",
